7e973b3896f634e38b423790c6fc3f113549b54d
Eric Pruitt <eric.pruitt@gmail.com>
2018-06-26 21:46:28 -0700

    Make various return lists in Python 3
    - Since I often use the Python REPL as a scratchpad or calculator,
      several functions have been modified so the user need not explicitly
      cast the returned generators as lists to see the results.

--- configuration/repl.py
+++ configuration/repl.py
@@ -148,6 +148,32 @@ def _configure_lazy_loader_proxies():
             setattr(repl_scope, name, LazyLoadedModuleProxy(name))
 
 
+def _install_eager_iterable_shims():
+    """
+    Modify "filter", "map", "range" and "zip" so they return lists in Python 3.
+
+    Since the Python REPL is often used as a scratchpad or calculator, several
+    functions have been modified so the user need not explicitly cast the
+    returned values as lists to see the results.
+    """
+    import sys
+
+    if sys.version_info < (3, ):
+        return
+
+    for function in [filter, map, range, zip]:
+        # A closure is used so each shim function sees a different "name".
+        def closure(name):
+            def shim(*args, **kwargs):
+                return list(getattr(builtins, name)(*args, **kwargs))
+
+            shim.__doc__ = function.__doc__
+            shim.__name__ = function.__name__
+            setattr(__import__(__name__), name, shim)
+
+        closure(function.__name__)
+
+
 try:
     _configure_lazy_loader_proxies()
 except Exception as e:
@@ -162,3 +188,8 @@ else:
         pass
 finally:
     del _configure_lazy_loader_proxies
+
+try:
+    _install_eager_iterable_shims()
+finally:
+    del _install_eager_iterable_shims

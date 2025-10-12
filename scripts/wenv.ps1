02eae0488000894eeab6ada5b4ea7a55c014108d
Eric Pruitt <epruitt@tesla.com>
2025-10-11 17:24:57 -0700

    Improve robustness of wenv.ps1 output and parsing

--- scripts/wenv.ps1
+++ scripts/wenv.ps1
@@ -2,6 +2,39 @@
 # Usage: wenv
 #
 # Dump Windows environment variables in POSIX shell format.
+function POSIXShellQuote {
+    param (
+        [string] $Text
+    )
+
+    return "'$($Text -replace '''', '''"''"''')'"
+}
+
+function IsPOSIXShellSafe {
+    param (
+        [string] $Text
+    )
+
+    $Text -notmatch "[|&;<>()$``\\`"'*?[#~!{}]|\s"
+}
+
 Get-ChildItem env: | ForEach-Object {
-    "$($_.Name)='$($_.Value -replace '''', '''"''"''')'"
+    $Name = $_.Name
+    $Value = $_.Value
+
+    # In practice, environment variables have few restrictions on what their
+    # names can be, but POSIX shells only support referencing them if they have
+    # a particular format. Quoting the variable names makes the assignments
+    # invalid if executed by an actual shell, but this at least reduces the
+    # likelihood of inadvertently executing code by sourcing/eval-ing the
+    # variables.
+    if ($Name -notmatch '^[A-Za-z_][A-Za-z0-9_]*$') {
+        $Name = POSIXShellQuote $Name
+    }
+
+    if (-not (IsPOSIXShellSafe $Value)) {
+        $Value = POSIXShellQuote $Value
+    }
+
+    "$Name=$Value"
 }
